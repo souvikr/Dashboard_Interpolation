85a5da09cd34a79a94a1a76a855e82d36c946c8d
souvikr <souvikroy5@gmail.com>
2019-06-11 20:55:21 +0530

    sent from Turing

--- check_database_values.py
+++ check_database_values.py
@@ -4,6 +4,6 @@ import sqlite3
 conn2 = sqlite3.connect('pm_database2.db')
 new_df = pd.read_sql_query("SELECT * FROM CAMPUS_PM",conn2)
 
-new_df.tail(10)
+print(new_df['TIMESTAMP','PM25_CONC'].tail(10))
 
 conn2.close()
