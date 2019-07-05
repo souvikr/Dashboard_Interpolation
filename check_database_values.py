9ec1a4e68ed6dd283e40dabdac3886268052935e
Souvik Roy <souvikroy5@gmail.com>
2019-07-06 00:10:58 +0530

    Update check_database_values.py

--- check_database_values.py
+++ check_database_values.py
@@ -2,10 +2,7 @@ import pandas as pd
 import sqlite3
 
 conn2 = sqlite3.connect('pm_database2.db')
-new_df = pd.read_sql_query("SELECT * FROM CAMPUS_PM",conn2)
-
-df = pd.DataFrame(new_df['TIMESTAMP'],new_df['PM25_CONC'])
-print(df.tail(10))
+new_df = pd.read_sql_query("DELETE FROM CAMPUS_PM WHERE TIMESTAMP=0",conn2)
 
 
 conn2.close()
